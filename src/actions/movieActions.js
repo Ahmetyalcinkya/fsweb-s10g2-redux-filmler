b49ba4e93eabf4e25484a34001512effe26bdfb4
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2023-09-26 17:39:15 +0300

    tamamlandı

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,5 +1,9 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const ADD_MOVIE = "Film ekle.";
 
 export const deleteMovie = (id) => {
-  return ({ type: DELETE_MOVIE, payload: id });
-}
+  return { type: DELETE_MOVIE, payload: id };
+};
+export const addMovie = (movie) => {
+  return { type: ADD_MOVIE, payload: movie };
+};
